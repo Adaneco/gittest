82aed006c03945b2c546c30e5b9ab84007db59b7
bramkok <intrinsiek@gmail.com>
2010-04-03 21:43:41 +0200

    a new html page as an index to make things prettier

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-This is the index.
-
-And this is new.
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title>This is a GIT test</title>
+	</head>
+	<body>
+		<h1>Hello</h1>
+		<p>This is a GIT test</p>
+	</body>
+</html>
