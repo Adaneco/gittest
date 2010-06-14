5b954ef3b822e2998fc0d0b329eaf7857fad6eb8
bramkok <intrinsiek@gmail.com>
2010-06-14 22:47:08 +0200

    Some CSS testing.

--- index.php
+++ index.php
@@ -8,13 +8,29 @@
 	<body>
 		<div class="bigtext">
 			<div class="bigtext_wrap">
-				<h1>Welcome to <br /><span>git test</span></h1>
-				<p>This is a nice <a href="#" title="Go to the GIT website">GIT</a> test.<br /> A repository to show how <a href="#" title="Go to the GIT website">GIT</a> works.</p>
+				<h1><span>ACME Inc.</span></h1>
+				<p>ACME Inc. Development blog for developers.</p>
 				<br />
 			</div>
 		</div>
 		<div class="wrap">
 			<div class="left">
+				<div class="post">
+					<div class="symbol">
+						&image;&infin;&real;
+					</div>
+					<a href="#" title="Blog post title" class="post_title"><h1>Eew, look at that code!</h1></a>
+					<small class="post_date">23-04-2010</small>
+					<small class="post_by">by <a href="#">Bram</a></small>
+					<small class="post_tags">tags: <a href="#">Technology</a>, <a href="#">CakePHP</a></small>
+					<p>Lorem ipsum dolor sit amet, <a href="#">consectetur</a> adipiscing elit. Nunc consequat pharetra dictum. Sed pharetra, lectus non ornare fringilla, erat leo tincidunt lectus, mattis fringilla ipsum magna vitae lectus. Pellentesque rhoncus pellentesque nisi. Suspendisse sodales lorem mauris. Phasellus gravida dignissim lacus, non mollis augue imperdiet et. Mauris tincidunt enim sit amet tellus ultricies malesuada. In hac habitasse platea dictumst. Suspendisse ut mi magna, non molestie urna.</p>
+					<h2>Performance tweaks</h2>
+					<p>Donec ac felis eget erat consectetur rutrum. Nullam gravida turpis sed lorem bibendum accumsan. Curabitur adipiscing elit quis metus molestie vehicula. Phasellus tristique lectus nec odio bibendum ultrices. Ut malesuada pharetra enim, vitae euismod mi sodales viverra. Nulla facilisi. Nam auctor pharetra luctus. Morbi id tellus dui, quis ornare diam. Ut tristique sapien vitae risus euismod id ullamcorper erat faucibus. Proin mauris sapien, molestie posuere laoreet elementum, tempor ac est. Duis mattis, justo quis mollis pellentesque, neque nunc faucibus risus, sit amet interdum libero orci eu ligula.</p>
+					<br />
+					<div class="post_footer">
+						Tell us what you think</small> 24 <a href="#" title="go to comments">comments</a> - by John
+					</div>
+				</div>
 				<div class="post">
 					<div class="flag">
 						Hello this is a warning flag. This content may look interesting but it is far from interesting. You're looking at just some random text to fill up this page.
@@ -28,31 +44,37 @@
 					<p>Donec ac felis eget erat consectetur rutrum. Nullam gravida turpis sed lorem bibendum accumsan. Curabitur adipiscing elit quis metus molestie vehicula. Phasellus tristique lectus nec odio bibendum ultrices. Ut malesuada pharetra enim, vitae euismod mi sodales viverra. Nulla facilisi. Nam auctor pharetra luctus. Morbi id tellus dui, quis ornare diam. Ut tristique sapien vitae risus euismod id ullamcorper erat faucibus. Proin mauris sapien, molestie posuere laoreet elementum, tempor ac est. Duis mattis, justo quis mollis pellentesque, neque nunc faucibus risus, sit amet interdum libero orci eu ligula.</p>
 					<br />
 					<div class="post_footer">
-						Every blogpost ends with a footer</small> 24 <a href="#" title="go to comments">comments</a> - by John
+						Tell us what you think</small> 24 <a href="#" title="go to comments">comments</a> - by John
 					</div>
 				</div>
 				<div class="post">
-					<h1>Very good title for a blog post</h1>
+					<a href="#" title="Blog post title" class="post_title"><h1>We have launched a new API for every developer out there!</h1></a>
+					<br />
+					<small class="post_date">23-04-2010</small>
+					<small class="post_by">by <a href="#">Bram</a></small>
+					<small class="post_tags">tags: <a href="#">Technology</a>, <a href="#">CakePHP</a></small>
+					<br /><br />
 					<p>Nullam mattis felis et ante pellentesque ac consequat neque hendrerit. Cras lacus augue, feugiat eget interdum eu, interdum vel lorem. Nullam diam felis, tempor ac rhoncus sit amet, lacinia in nunc. Donec iaculis scelerisque eros, ut fringilla libero semper vel. Etiam euismod, neque vel tempor feugiat, lacus lacus pulvinar sapien, sed feugiat ante nibh quis eros. Suspendisse lobortis facilisis ligula, vel blandit eros ultricies a. Praesent tempor nibh dapibus sapien imperdiet et viverra elit facilisis. Vivamus malesuada, magna quis dapibus dictum, lacus enim commodo velit, nec adipiscing nibh velit non justo. Duis non urna sem. Vivamus vulputate ullamcorper metus eu vestibulum.</p>
 					<h2>Less valuable assests</h2>
 					<p>Phasellus eget risus nec velit condimentum accumsan vel ut eros. Vivamus a lorem et nunc varius pellentesque in et turpis. Duis suscipit orci non erat porttitor vitae pellentesque nunc sollicitudin. Integer imperdiet enim ac turpis ornare venenatis. Duis auctor, quam sed pretium aliquet, est lacus sagittis nibh, a fringilla velit nulla non sem. Nullam velit mi, faucibus vel suscipit non, facilisis in nibh. Morbi condimentum lectus ut ante lacinia commodo non ut magna. Praesent sed ligula libero. Proin suscipit dictum ligula ac condimentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+					<p>Fusce adipiscing elementum leo, a eleifend tellus pellentesque mattis. Fusce nec nulla turpis. Integer ante velit, scelerisque vitae placerat at, aliquet nec erat. Nulla dignissim eleifend risus, non porttitor lacus sollicitudin nec. Mauris nec nisl leo, porta interdum diam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris eleifend vehicula venenatis. Pellentesque vitae sapien in diam pharetra cursus. Fusce nec metus dolor, a sodales elit. Integer convallis egestas dui, non varius metus ultricies eget. Ut ac odio et dui porta egestas sit amet id sapien. Morbi ultrices, magna eu porta tincidunt, ipsum arcu viverra magna, egestas dignissim est magna non nunc. Ut ultricies magna sit amet ipsum interdum in interdum massa fringilla. Praesent viverra mi eu sapien posuere et accumsan diam pharetra. Nam fermentum lobortis suscipit. Duis nibh ligula, sodales sed ultricies sit amet, cursus id neque.</p>
+					<p>Mauris libero lacus, fringilla quis vehicula quis, egestas ultricies lacus. Quisque faucibus lectus nec nunc molestie mattis. Phasellus eget quam enim. Sed sapien elit, semper vel pulvinar sed, sodales ac erat. In porta, tortor sit amet tempor congue, purus turpis eleifend quam, rutrum mollis magna nisi non velit. In quis ligula ante, sit amet laoreet libero. Curabitur a erat non metus sollicitudin convallis quis ut elit. Suspendisse quis nisl orci, non sagittis enim. Duis leo nibh, viverra non ullamcorper ullamcorper, sollicitudin eu mi. Praesent erat felis, lobortis eu laoreet nec, laoreet quis augue. Curabitur a nisl vitae arcu euismod pellentesque. Vivamus eleifend, urna faucibus condimentum ultricies, odio enim bibendum turpis, et imperdiet dui nibh nec est. Donec eget arcu diam, ut ultricies lacus. Nullam fermentum orci eget enim varius eu ornare tortor molestie. Maecenas vitae risus nibh. Morbi semper ligula ornare odio mollis a commodo felis elementum.</p>
+					<p>Suspendisse et turpis ligula. Maecenas consectetur augue leo, eget adipiscing ipsum. Phasellus placerat est non risus elementum euismod. Aenean non tortor vel metus rhoncus ornare. Sed orci velit, dignissim id euismod vitae, dignissim et turpis. Aenean sed leo magna. Sed id erat sed ligula lobortis fermentum. Integer posuere, mi a lacinia hendrerit, enim lorem placerat nibh, non blandit lacus sapien vel leo. Pellentesque quis odio erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
-				<p>Fusce adipiscing elementum leo, a eleifend tellus pellentesque mattis. Fusce nec nulla turpis. Integer ante velit, scelerisque vitae placerat at, aliquet nec erat. Nulla dignissim eleifend risus, non porttitor lacus sollicitudin nec. Mauris nec nisl leo, porta interdum diam. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris eleifend vehicula venenatis. Pellentesque vitae sapien in diam pharetra cursus. Fusce nec metus dolor, a sodales elit. Integer convallis egestas dui, non varius metus ultricies eget. Ut ac odio et dui porta egestas sit amet id sapien. Morbi ultrices, magna eu porta tincidunt, ipsum arcu viverra magna, egestas dignissim est magna non nunc. Ut ultricies magna sit amet ipsum interdum in interdum massa fringilla. Praesent viverra mi eu sapien posuere et accumsan diam pharetra. Nam fermentum lobortis suscipit. Duis nibh ligula, sodales sed ultricies sit amet, cursus id neque.</p>
-
-				<p>Mauris libero lacus, fringilla quis vehicula quis, egestas ultricies lacus. Quisque faucibus lectus nec nunc molestie mattis. Phasellus eget quam enim. Sed sapien elit, semper vel pulvinar sed, sodales ac erat. In porta, tortor sit amet tempor congue, purus turpis eleifend quam, rutrum mollis magna nisi non velit. In quis ligula ante, sit amet laoreet libero. Curabitur a erat non metus sollicitudin convallis quis ut elit. Suspendisse quis nisl orci, non sagittis enim. Duis leo nibh, viverra non ullamcorper ullamcorper, sollicitudin eu mi. Praesent erat felis, lobortis eu laoreet nec, laoreet quis augue. Curabitur a nisl vitae arcu euismod pellentesque. Vivamus eleifend, urna faucibus condimentum ultricies, odio enim bibendum turpis, et imperdiet dui nibh nec est. Donec eget arcu diam, ut ultricies lacus. Nullam fermentum orci eget enim varius eu ornare tortor molestie. Maecenas vitae risus nibh. Morbi semper ligula ornare odio mollis a commodo felis elementum.</p>
-
-				<p>Suspendisse et turpis ligula. Maecenas consectetur augue leo, eget adipiscing ipsum. Phasellus placerat est non risus elementum euismod. Aenean non tortor vel metus rhoncus ornare. Sed orci velit, dignissim id euismod vitae, dignissim et turpis. Aenean sed leo magna. Sed id erat sed ligula lobortis fermentum. Integer posuere, mi a lacinia hendrerit, enim lorem placerat nibh, non blandit lacus sapien vel leo. Pellentesque quis odio erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 			</div>
 			<div class="right">
 				<div class="sidebar">
 					<ul>
-						<li><a href="#">Archive</a></li>
-						<li><a href="#">Tags</a></li>
-						<li><a href="#">RSS</a></li>
+						<li><span>&deg;</span><a href="#">Archive</a></li>
+						<li><span>&deg;</span><a href="#">Tags</a></li>
+						<li><span>&deg;</span><a href="#">RSS</a></li>
 					</ul>
-					<p>This is just another piece of text to make it look like something it isn't.</p>
+					<p>For more information visit: <br /> <a href="#" title="Visit our website">www.ACMEinc.com</a></p>
 				</div>
 			</div>
+		</div>
+		<div class="footer">
+			<div class="footer_wrap"></div>
 		</div>
 	</body>
 </html>
